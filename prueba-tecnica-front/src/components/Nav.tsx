0dcdd78fb0c0b3942a7dee2d123118cd48b80216
victoriaQuintero <victoria170460@gmail.com>
2024-03-19 15:37:24 -0400

    Nav/responsive-nav

---
+++ prueba-tecnica-front/src/components/Nav.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image"
+import Logo from "../../public/assets/Logo.svg"
+import Menu from "../../public/assets/Menu.svg"
+
+export default function Nav() {
+
+    const itemsNav = [
+        {
+            name: "Home"
+        },
+        {
+            name: "Products"
+        },
+        {
+            name: "Resources"
+        },
+        {
+            name: "Pricing"
+        },
+    ]
+
+    return (
+        <div className="flex items-center justify-center">
+            <div className="w-full flex items-center justify-between p-4 lg:px-8 lg:w-[90%]">
+                <div className="flex items-center gap-10">
+                    <Image src={Logo} alt="logo" />
+                    <nav className="hidden lg:flex lg:items-center lg:gap-8">
+                        {
+                            itemsNav.map((item, index) => (
+                                <p key={index} className="text-[#475467] text-base font-semibold">{item.name}</p>
+                            ))
+                        }
+                    </nav>
+                </div>
+                <Image className="lg:hidden" src={Menu} alt="Menu" />
+                <div className="hidden lg:flex lg:gap-3">
+                    <button className="text-[#475467] text-base font-semibold py-[10px] px-4 rounded-lg">
+                        Log in
+                    </button>
+                    <button className="bg-[#7F56D9] text-white text-base font-semibold py-[10px] px-4 rounded-lg">
+                        Sing up
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
